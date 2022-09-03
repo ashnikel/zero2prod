73856c6e9b80aab27ae82e6ab516d29d0f08c037
Victor Medvedik <ashnikel@gmail.com>
2022-09-03 12:19:04 +0300

    Read config file

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -25,7 +25,7 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
-    let client =  reqwest::Client::new();
+    let client = reqwest::Client::new();
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -49,7 +49,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email")
+        ("", "missing both name and email"),
     ];
 
     for (invalid_body, error_message) in test_cases {
