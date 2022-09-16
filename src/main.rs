5060da3fdc5e196335e1c1b0b5bdacf824f301bb
Victor Medvedik <ashnikel@gmail.com>
2022-09-16 18:00:22 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -12,8 +13,9 @@ async fn main() -> std::io::Result<()> {
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
 
     // We have removed the hard-coded `8000` - it's now coming from our settings!
